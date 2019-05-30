872c7c51f652352970eaecf2a24e1fd99f8ad6b0
marvin tang <marintang@126.com>
2019-05-30 20:13:03 +0800

    django introduction

--- django_introduction/blog/urls.py
+++ django_introduction/blog/urls.py
@@ -6,4 +6,6 @@ import blog.views
 urlpatterns = [
     path("hello_world", blog.views.hello_world), #如果地址含有hello_world，就转发到视图里
     path("content",blog.views.article_content),
+    path('index', blog.views.get_index_page),
+    path('detail', blog.views.get_detail_page),
 ]
--- django_introduction/blog/views.py
+++ django_introduction/blog/views.py
@@ -19,3 +19,23 @@ def article_content(request):
                                                                                                  article_id,
                                                                                                  publish_date)
     return HttpResponse(return_str)
+
+
+def get_index_page(request):
+    all_article = Article.objects.all()     #取出所有文章
+    return render(request, 'blog/index.html',        #render：把模板系统和数据进行渲染和返回
+                  {
+                      'article_list' : all_article    #字典
+                  }
+                  )
+
+
+def get_detail_page(request):
+    curr_article = Article.objects.all()[4]
+    section_list = curr_article.content.split('\n')     #文章内容按空行切分开（按跨行符切分成一个列表）
+    return render(request, 'blog/detail.html',        #render：把模板系统和数据进行渲染和返回
+                  {
+                      'curr_article' : curr_article,    #字典
+                      'section_list' : section_list     #把它作为变量传入模板系统里
+                  }
+                  )
